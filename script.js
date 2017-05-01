8ffea7e6eb5a94289f65ef6aae8e256ae346d569
Ian Zolitor <ianzolitor@gmail.com>
2017-05-01 16:49:57 -0400

    function working

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
 var neighborhoods = document.getElementsByClassName("neighborhood")
 var searchButton = document.getElementsByClassName("search-button")[0];
 var input = document.getElementById("business")
+var locationSelect = document.getElementsByClassName("location-select")[0]
 
+
+var clickNeighborhood = ""
+
+
+
+for (var i = 0; i < neighborhoods.length; i++) {
+	neighborhoods[i].addEventListener("mouseover", function(event){
+		event.target.style.cursor = "pointer"
+	})
+}
+
+
+for (let i = 0; i < neighborhoods.length; i++) {
+	neighborhoods[i].addEventListener("click", function(event){
+		if(i===0){
+			clickNeighborhood = "Germantown"
+		}
+		else if(i===1){
+			clickNeighborhood = "West Philadelphia"
+		}
+		else if(i===2){
+			clickNeighborhood = "Fishtown"
+		}
+		else if(i===3){
+			clickNeighborhood = "Manayunk"
+		}
+		else if(i===4){
+			clickNeighborhood = "Center City"
+		}
+		locationSelect.innerHTML = "Find Businesses in " + clickNeighborhood
+		console.log(clickNeighborhood)
+	})
+}
 
 searchButton.addEventListener("mouseover", function(event){
 	event.target.style.cursor = "pointer"
 })
-
-
 
 searchButton.addEventListener("click", searchBusiness)
 
@@ -15,11 +47,21 @@ searchButton.addEventListener("click", searchBusiness)
 function searchBusiness() {
 
 var inputText = input.value
-console.log(inputText)
-
-
-
+ $.ajax({
+         url: "https://yelp-search.herokuapp.com/search",
+         method: "GET",
+         data: {
+         	term: inputText,
+         	location: clickNeighborhood
+        },
+         success: function(response){
+             console.log(response)
+         }
+     })
 }
+
+
+
 
 
  // $.ajax({
