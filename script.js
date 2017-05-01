b4d24822f0589730d23994ad61f0d8dfdb998f72
Ian Zolitor <ianzolitor@gmail.com>
2017-05-01 17:35:36 -0400

    results on html

--- script.js
+++ script.js
@@ -2,10 +2,18 @@ var neighborhoods = document.getElementsByClassName("neighborhood")
 var searchButton = document.getElementsByClassName("search-button")[0];
 var input = document.getElementById("business")
 var locationSelect = document.getElementsByClassName("location-select")[0]
+var busName = document.getElementsByClassName("name")[0]
+var busAddress = document.getElementsByClassName("address")[0]
+var busImage = document.getElementsByClassName("image")[0]
 
 
 var clickNeighborhood = ""
 
+// document.body.addEventListener("keypress", function (event) {
+// 	if (event.keyCode===13){
+// 		searchBusiness()
+// 	}
+// })
 
 
 for (var i = 0; i < neighborhoods.length; i++) {
@@ -55,7 +63,14 @@ var inputText = input.value
          	location: clickNeighborhood
         },
          success: function(response){
-             console.log(response)
+         	console.log(response)
+       
+			response.businesses.forEach(function(object){
+            busName.innerHTML += "<li>" + object.name + "</li>"
+
+
+            })
+             
          }
      })
 }
